c9874fdb376850c7d9b9e3bad6f8a118c6af2273
Steven <steven.guzman@yachaytech.edu.ec>
2022-04-08 00:45:34 -0500

    Added new class.

---
+++ LargestQuakes.java
@@ -0,0 +1,10 @@
+import java.util.*;
+/**
+ * Write a description of class LargestQuakes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LargestQuakes {
+
+}
